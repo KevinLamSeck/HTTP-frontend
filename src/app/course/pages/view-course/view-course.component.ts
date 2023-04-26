a0fc5409fc6fb6e89b8691f70d42d5788ca4cb66
jimmy <jimmy.lelievre@poe.aelion.biz>
2023-04-26 11:09:52 +0200

    Add total time modules in view course

--- src/app/course/pages/view-course/view-course.component.ts
+++ src/app/course/pages/view-course/view-course.component.ts
@@ -9,8 +9,8 @@ import { CourseType } from '../../types/course-type';
 export class ViewCourseComponent implements OnInit {
   public course: CourseType;
   public user: any;
-  public totalTime: Array<any> = [];
-  public total: any;
+  public addTime: Array<any> = [];
+  public totalTime: any;
 
   constructor() {
     this.course = JSON.parse(sessionStorage.getItem('ModifiedCourse') + '');
@@ -18,25 +18,23 @@ export class ViewCourseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log(this.course.modules);
     this.course.modules?.forEach((module) => {
-      this.totalTime.push(module.totalTime);
+      this.addTime.push(module.totalTime);
     });
 
-    const totalMinutes = this.totalTime.reduce((acc, curr) => {
+    const totalMinutes = this.addTime.reduce((acc, curr) => {
       const [minutes, seconds] = curr
         .split(':')
         .map((part: string) => parseInt(part));
       return acc + minutes * 60 + seconds;
     }, 0);
+
     const hours = Math.floor(totalMinutes / 60)
       .toString()
       .padStart(2, '0');
 
     const minutes = (totalMinutes % 60).toString().padStart(2, '0');
 
-    this.total = `${hours} ${minutes}min`;
-
-    console.log(this.total);
+    this.totalTime = `${hours}h ${minutes}min`;
   }
 }

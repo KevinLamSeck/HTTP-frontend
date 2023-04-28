13eebe8d4bbe28a68848527d9bb8c85ffa89cec6
Platypuus <clement8.baille@gmail.com>
2023-04-28 10:49:54 +0200

    recovery and add module css update

--- src/app/user/auth/login/login.component.ts
+++ src/app/user/auth/login/login.component.ts
@@ -1,49 +1,49 @@
-import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 import {
   AbstractControl,
   FormControl,
   FormGroup,
   Validators,
-} from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { LocalStorageStrategy } from 'src/app/core/store/local-storage-strategy';
-import { SessionStorageStrategy } from 'src/app/core/store/session-storage-strategy';
-import { ToastService } from 'src/app/core/toast.service';
-import { environment } from '../../../../environments/environment';
-import { RecoveryPasswordComponent } from '../../dialogs/recovery-password/recovery-password.component';
-import { RecoveryComponent } from '../../dialogs/recovery/recovery.component';
-import { UserService } from '../../services/user.service';
+} from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { LocalStorageStrategy } from "src/app/core/store/local-storage-strategy";
+import { SessionStorageStrategy } from "src/app/core/store/session-storage-strategy";
+import { ToastService } from "src/app/core/toast.service";
+import { environment } from "../../../../environments/environment";
+import { RecoveryPasswordComponent } from "../../dialogs/recovery-password/recovery-password.component";
+import { RecoveryComponent } from "../../dialogs/recovery/recovery.component";
+import { UserService } from "../../services/user.service";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
   public form: FormGroup = new FormGroup({});
   public showPassword: boolean = false;
 
   public stayConnected: boolean =
-    environment.storage.auth.strategy !== 'session';
+    environment.storage.auth.strategy !== "session";
 
   constructor(
     private _userService: UserService,
     private _router: Router,
     private _dialog: MatDialog,
     private _toastService: ToastService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    const loginControl: AbstractControl = new FormControl('', [
+    const loginControl: AbstractControl = new FormControl("", [
       Validators.required,
     ]);
-    const passwordControl: AbstractControl = new FormControl('', [
+    const passwordControl: AbstractControl = new FormControl("", [
       Validators.required,
     ]);
 
-    this.form.addControl('login', loginControl);
-    this.form.addControl('password', passwordControl);
+    this.form.addControl("login", loginControl);
+    this.form.addControl("password", passwordControl);
   }
 
   passwordToggle(): void {
@@ -65,16 +65,16 @@ export class LoginComponent implements OnInit {
   openRecovery(): void {
     this._dialog
       .open(RecoveryComponent, {
-        height: 'flex',
-        width: 'flex',
+        height: "flex",
+        width: "30vw",
       })
       .afterClosed()
       .subscribe((result: HttpResponse<any> | undefined) => {
         // console.log(result);
         if (result !== undefined) {
           this._dialog.open(RecoveryPasswordComponent, {
-            height: 'flex',
-            width: 'flex',
+            height: "flex",
+            width: "flex",
             data: result,
           });
         }
@@ -84,15 +84,15 @@ export class LoginComponent implements OnInit {
   onSubmit(): void {
     this._userService.authenticate(this.form.value).subscribe({
       next: (response: HttpResponse<any>) => {
-        this._router.navigate(['/']);
+        this._router.navigate(["/"]);
       },
       error: (error: any) => {
         const messageErrorLogin: string = `Désolé vous avez rentrer des identifiants incorrects !`;
         this._toastService.show(messageErrorLogin);
       },
       complete: () => {
-        this.form.controls['login'].setValue('');
-        this.form.controls['password'].setValue('');
+        this.form.controls["login"].setValue("");
+        this.form.controls["password"].setValue("");
         const messageSucessLogin: string = `Welcome !`;
         this._toastService.show(messageSucessLogin);
       },

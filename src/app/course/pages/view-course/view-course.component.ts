d18b71cebf07ac4d1a6e4a9476536545194dbac0
jimmy <jimmy.lelievre@poe.aelion.biz>
2023-05-03 10:30:13 +0200

    fix total time in view course

--- src/app/course/pages/view-course/view-course.component.ts
+++ src/app/course/pages/view-course/view-course.component.ts
@@ -16,6 +16,13 @@ export class ViewCourseComponent implements OnInit {
     this.course = JSON.parse(sessionStorage.getItem('ModifiedCourse') + '');
     this.user = JSON.parse(localStorage.getItem('memberData') + '');
   }
+  time() {
+    if (this.totalTime === '00h 00min') {
+      ('');
+    } else {
+      this.totalTime + 'total length';
+    }
+  }
 
   ngOnInit(): void {
     this.course.modules?.forEach((module) => {
@@ -36,6 +43,6 @@ export class ViewCourseComponent implements OnInit {
     const minutes = (totalMinutes % 60).toString().padStart(2, '0');
 
     this.totalTime = `${hours}h ${minutes}min`;
-    console.log(this.totalTime);
+    console.log(this.addTime);
   }
 }

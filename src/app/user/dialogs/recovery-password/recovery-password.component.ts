9e787ed5608dda095848e65d71cce24252fb720d
Kevin Lam Seck <kevin.lamseck@gmail.com>
2023-05-03 22:27:42 +0200

    UX - Add click to clickboard

--- src/app/user/dialogs/recovery-password/recovery-password.component.ts
+++ src/app/user/dialogs/recovery-password/recovery-password.component.ts
@@ -1,6 +1,7 @@
 import { HttpResponse } from '@angular/common/http';
-import { Component,Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ToastService } from 'src/app/core/toast.service';
 
 @Component({
   selector: 'app-recovery-password',
@@ -11,13 +12,22 @@ export class RecoveryPasswordComponent implements OnInit {
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: HttpResponse<any>,
-    public dialogRef: MatDialogRef<RecoveryPasswordComponent>) { }
+    public dialogRef: MatDialogRef<RecoveryPasswordComponent>,
+    private _toastService: ToastService) { }
 
   ngOnInit(): void {
   }
 
   onClose(): void {
     this.dialogRef.close()
+  }
+
+  copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text).then(() => {
+      this._toastService.show('Password copied to clipboard', 'Close');
+    }).catch((err) => {
+      console.error('Failed to copy text:', err);
+    });
   }
 
 }
